92d1228d69a0f60abc73b99bb5b8fd77488d813e
Nikolay Ivanov <nikolay.ivanov@flatrocktech.com>
2023-11-15 14:02:19 +0200

    remove app.css. implement simple navigation. Add example logo

--- realistic-vision-app/src/App.js
+++ realistic-vision-app/src/App.js
@@ -1,5 +1,4 @@
 import Navigation from "./components/navigation/Navigation";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- realistic-vision-app/src/components/navigation/Navigation.js
+++ realistic-vision-app/src/components/navigation/Navigation.js
@@ -9,18 +9,36 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 function Navigation({ expand }) {
   return (
     <>
-      <Navbar className="bg-body-tertiary">
-        <Container>
+      <Navbar className="bg-body-transparent bg-white" expand="lg" sticky="top">
+        <Container fluid>
           <Navbar.Brand href="#home">
             <img
-              alt=""
-              src="/img/logo.svg"
+              alt="realistic vision logo"
+              src="../../assets/logos/light-bulb.png"
               width="30"
               height="30"
               className="d-inline-block align-top"
             />
-            React Bootstrap
+            <p className="d-inline">Realistic Vision</p>
           </Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                Welcome
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
         </Container>
       </Navbar>
     </>
@@ -28,48 +46,3 @@ function Navigation({ expand }) {
 }
 
 export default Navigation;
-
-/*<Container fluid>
-            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                  <NavDropdown
-                    title="Dropdown"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-          */
