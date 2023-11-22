9d2438548989dd885c4ea83e1c199f7eab5ad533
Nikolay Ivanov <nikolay.ivanov@flatrocktech.com>
2023-11-22 16:48:58 +0200

    Add React-Measure for more finished look. Improve gallery to show images more accurate

--- realistic-vision-app/src/components/modals/ImageModal.js
+++ realistic-vision-app/src/components/modals/ImageModal.js
@@ -1,28 +1,30 @@
-import Modal from 'react-bootstrap/Modal';
+import Modal from "react-bootstrap/Modal";
 
 function ImageModal({ ...props }) {
-	return (
-		<Modal
-			{...props}
-			size="lg"
-			aria-labelledby="contained-modal-title-vcenter"
-			centered
-			style={{ padding: 0 }}
-		>
-			<Modal.Header closeButton></Modal.Header>
-			<Modal.Body>
-				<img
-					src={props.img?.src}
-					alt={`mansory ${props.img?.id}`}
-					style={{
-						width: '100%',
-						display: 'block',
-						objectFit: 'fill',
-					}}
-				/>
-			</Modal.Body>
-		</Modal>
-	);
+  const imageTitle =
+    props.img?.title == "" ? "Default image title" : props.img?.title;
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      style={{ padding: 0 }}
+    >
+      <Modal.Header closeButton>{imageTitle}</Modal.Header>
+      <Modal.Body style={{ padding: 0 }}>
+        <img
+          src={props.img?.src}
+          alt={`mansory ${props.img?.id}`}
+          style={{
+            width: "100%",
+            display: "block",
+            objectFit: "fill",
+          }}
+        />
+      </Modal.Body>
+    </Modal>
+  );
 }
 
 export default ImageModal;
--- realistic-vision-app/src/pages/GalleryPage.js
+++ realistic-vision-app/src/pages/GalleryPage.js
@@ -1,27 +1,25 @@
-import GalleyMasonry from '../components/mansoryGallery/GalleryMasonry';
-import { useState } from 'react';
+import GalleyMasonry from "../components/mansoryGallery/GalleryMasonry";
+import { useState } from "react";
 //import fs from 'fs';
 
 function getRandomInt(max) {
-	return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 const range = (start, stop, step) =>
-	Array.from({ length: (stop - start) / step + 1 }, (_, i) => {
-		return {
-			id: start + i * step,
-			src: `https://picsum.photos/${getRandomInt(3)}000/${getRandomInt(
-				2
-			)}000`,
-			title: '',
-		};
-	});
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => {
+    return {
+      id: start + i * step,
+      src: `https://picsum.photos/${getRandomInt(2)}000/${getRandomInt(1)}000`,
+      title: `Image: ${i + 1}`,
+    };
+  });
 //
 function GalleryPage() {
-	const [images, setImages] = useState(range(1, 50, 2));
-	//TODO: Dynamically add them from assets folder
-	//const files = fs.readdirSync('/assets/photos/');
-	return <GalleyMasonry imageData={images} />;
+  const [images, setImages] = useState(range(1, 20, 1));
+  //TODO: Dynamically add them from assets folder
+  //const files = fs.readdirSync('/assets/photos/');
+  return <GalleyMasonry imageData={images} />;
 }
 
 export default GalleryPage;
