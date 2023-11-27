976c11a88720323a2954c8db4afe606a0a3a0766
Nikolay Ivanov <nikolay.ivanov@flatrocktech.com>
2023-11-27 10:59:13 +0200

    Clean about page from console log

--- realistic-vision-app/src/pages/AboutUsPage.js
+++ realistic-vision-app/src/pages/AboutUsPage.js
@@ -2,7 +2,6 @@ import { useOutletContext } from "react-router-dom";
 
 function AboutUsPage() {
   const [content] = useOutletContext();
-  console.log(content);
   return <h1 className="text-white">This is about page</h1>;
 }
 
