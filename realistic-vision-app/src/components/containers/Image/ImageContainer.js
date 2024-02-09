b862c4108fb10c0413a99acd337c33717e725257
Nikolay Ivanov <nikolay.ivanov@flatrocktech.com>
2024-02-09 15:21:01 +0200

    add carousel section image container and nav context

---
+++ realistic-vision-app/src/components/containers/Image/ImageContainer.js
@@ -0,0 +1,13 @@
+import Image from "react-bootstrap/Image";
+
+function ImageContainer({ src }) {
+  return (
+    <Image
+      style={{ width: "100%", height: "100%", objectFit: "cover" }}
+      src={src}
+      alt="full width"
+    />
+  );
+}
+
+export default ImageContainer;
---
+++ realistic-vision-app/src/components/containers/carousels/CarouselSection.js
@@ -0,0 +1,38 @@
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+function CarouselSection({ components = [], defaultActiveIndex = 0 }) {
+  const handleSlideChange = (swiper) => {
+    console.log(swiper);
+  };
+
+  const handleonActiveIndexChange = () => {};
+
+  return (
+    <Swiper
+      style={{ width: "100vw", height: "100vh" }}
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={0}
+      slidesPerView={1}
+      onActiveIndexChange={(swipper) => {
+        console.log(swipper.navigation);
+        console.log(swipper.activeIndex);
+      }}
+      navigation
+      scrollbar={{ draggable: true }}
+      onSlideChange={handleSlideChange}
+    >
+      {components.map((component, index) => (
+        <SwiperSlide style={{ width: "100vw", height: "100vh" }} key={index}>
+          {component}
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
+
+export default CarouselSection;
