edf128a45f5d0f8df4e205bf72b5caf40e4a0c70
Nikolay Ivanov <nikolay.ivanov@flatrocktech.com>
2024-11-17 22:55:20 +0200

    Update AboutUsPage.js

--- realistic-vision-app/src/pages/AboutUsPage.js
+++ realistic-vision-app/src/pages/AboutUsPage.js
@@ -35,16 +35,16 @@ function AboutUsPage() {
 					className="d-grid"
 					style={{ marginLeft: '55px', marginRight: '55px' }}
 				>
-					{AboutUs?.Sections?.map((section) => {
+					{AboutUs?.Sections?.map((section, aboutUsIndex) => {
 						return (
-							<div className="row">
+							<div className="row" id={aboutUsIndex}>
 								<div className="col-sm-12 col-md-6 col-lg-7 col-xl-7 p-0 m-0"></div>
 								<div className="col-sm-12 col-md-6 col-lg-4 col-xl-4 p-0 m-0">
 									{section?.paragraphs?.map(
-										(paragraph, index) => {
+										(paragraph, paragraphIndex) => {
 											return (
 												<p
-													id={index}
+													id={paragraphIndex}
 													style={{
 														textAlign: 'left',
 														marginBottom: '5px',
